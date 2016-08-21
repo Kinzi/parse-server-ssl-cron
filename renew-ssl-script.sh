f5c8783b21da8b4794965c504d5ba1b26e999de4
Sebastian Kinzlinger <webmarken@gmail.com>
2016-08-21 21:16:45 +0200

    Update renew-ssl-script.sh

--- renew-ssl-script.sh
+++ renew-ssl-script.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
-# Renew SSL cert
+# stop nginx
+/etc/init.d/nginx stop
+
+# check for new cert
 /opt/letsencrypt/letsencrypt-auto renew >> /var/log/le-renew.log
 
-# combine letsencrypt files for mongo
-cat /etc/letsencrypt/archive/<YOURDOMAIN>/{fullchain1.pem,privkey1.pem} | tee /etc/ssl/mongo.pem
+# combine latest letsencrypt files for mongo
 
-# set rights for mongo.pem 
+# find latest fullchain*.pem
+newestFull=$(ls -v /etc/letsencrypt/archive/server.packtor.com/fullchain*.pem | tail -n 1)
+echo "$newestFull"
+
+# find latest privkey*.pem
+newestPriv=$(ls -v /etc/letsencrypt/archive/server.packtor.com/privkey*.pem | tail -n 1)
+echo "$newestPriv"
+
+# combine to mongo.pem
+cat {$newestFull,$newestPriv} | tee /etc/ssl/mongo.pem
+
+# set rights for mongo.pem
 chmod 600 /etc/ssl/mongo.pem
 chown mongodb:mongodb /etc/ssl/mongo.pem
 
 # restart mongo
 /sbin/restart mongod
 
-# restart nginx
-/etc/init.d/nginx reload
-
-
-#### parse user crontab #####
-# Restart pm2 with your parse user NOT root # make sure log folder exists
-12 2 * * 2 /usr/bin/pm2 /sbin/restart 0 >> ~/logs/pm2/pm2-cron.log 2>&1
+# start nginx
+/etc/init.d/nginx start
